f02e90e0cd5ae5192e4885feeb0ca528ffcf2812
neolao <contact@neolao.com>
2020-04-21 23:04:50 +0100

    fix

--- index.js
+++ index.js
@@ -6,40 +6,40 @@ const height = 480;
 
 function displayImageBuffer(img){
   const buf = Buffer.alloc(width * height, 0x00);
+  //const buf = Buffer.alloc(width * height, 0xFF);
 
   for (let y = 0; y < height; y++) {
     for (let x = 0; x < width; x++){
       const pixel = img.getPixel(x, y);
       if (pixel > 0) {
-        const i = Math.round((y * width + x) / 8);
-        buf[i] = 0xFF;
+        const i = Math.floor((y * width + x) / 8);
+        buf[i] |= 0x80 >> (x % 8);
+        //buf[i] &= ~(0x80 >> (x % 8));
       }
     }
   }
   epd.display(buf);
 }
 
-
 async function main() {
   epd.init();
   //epd.clear();
 
   const font = `${__dirname}/fonts/DejaVuSansMono.ttf`;
+  const fontSize = 10;
+  const maxCol = Math.floor(width / (fontSize / 1.25));
+  const maxRow = Math.floor(height / fontSize);
 
   const img = await gd.create(width, height);
   const white = img.colorAllocate(255, 255, 255);
   const black = img.colorAllocate(0, 0, 0);
   img.rectangle(0, 0, width, height, white);
-  const textColor = img.colorAllocate(0, 0, 0);
-  img.stringFT(textColor, font, 64, 0, 300, 60, 'Hello');
-  img.setPixel(51, 51, 0x000000);
-  img.rectangle(20, 20, 280, 180, black);
+  img.stringFT(black, font, fontSize, 0, 0, 16, `Screen size: ${maxCol} x ${maxRow}`);
+  img.stringFT(black, font, fontSize, 0, 0, fontSize * maxRow, " ".repeat(maxCol - 1) + "┘");
+  //img.rectangle(20, 20, 280, 180, black);
 
   displayImageBuffer(img);
 
-  await img.savePng(`${__dirname}/file.png`, 0);
   img.destroy();
-
-//epd.clear();
 }
 main();
